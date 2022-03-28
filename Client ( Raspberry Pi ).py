bdc85d9a37122f360dc4d9fa8a80952261dc7e76
VR Driving Simulator <101948671+drivingsimulator22@users.noreply.github.com>
2022-03-28 14:01:30 +0200

    Update Client ( Raspberry Pi ).py

--- Client ( Raspberry Pi ).py
+++ Client ( Raspberry Pi ).py
@@ -1,32 +1,40 @@
-# Import socket module
-import socket           
- 
-# Create a socket object
-socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)        
- 
-# Define the port on which you want to connect
-port = 12345   
-# Defining the variables            
-pitchyaw=0
-roll=0
-pitch=0
-yaw=0
-# Counter to avoid first few readings that have error
-delayer=0
-# connect to the server on local computer
-socket1.connect(('192.168.137.1', port))
+###### Functions Definitions:
+#######################################################################
+# Function Name: clientConnect() 
+# Description:
+# function is used to connect to the server from the raspberry pi
+# to the computer. This is implemented using the built-in python socket module.
+# We do this by defining a socket with an unused port. and connecting to the PC's
+# IP address.
+#######################################################################
+# Function Name: takeReadings() 
+# function takes the readings sent from the PC server from SIMTOOLS.
+# It then slices the recieved readings to get the roll,pitch and yaw angles.
+# the angles are then returned to be used in calculations later.
+######
+import socket
+def clientConnect():
+    # Create a socket object
+    global socketClient
+    socketClient = socket.socket(socket.AF_INET,socket.SOCK_STREAM)        
+    # Define the port on which you want to connect
+    port = 5050   
+    # connect to the server on local computer
+    socketClient.connect(('192.168.130.220', port))
+    print("Connected to host PC")
+def takeReadings():
+    #Defining the variables            
+    recieved = socketClient.recv(32768).decode()     ##### Put the recieved readings into variable
+    #### The initial reading sent from PC is in format: "0,0,0,"
+    #### so we split the string with "," as delimiter
+    #### and assign each value to the appropriate angle.
+    roll_recieved = recieved.split(",")[0]            ### Roll
+    pitch_recieved = recieved.split(",")[1]           ### Pitch
+    yaw_recieved = recieved.split(",")[2]             ### Yaw
+    ## The following if conditional is to remove all errors and noise
+    ## from our readings because sometimes we get two readings stuck together.
+    if int(yaw_recieved)<1024:                              
+        return [roll_recieved,pitch_recieved,yaw_recieved]
+clientConnect()
 while True:
-    recieved = socket1.recv(32768).decode()     ##### Put the recieved readings into variable
-    if "." in recieved:                         ##### "." is a delimiter we put in the readings to identify first element,so if we have "." in reading it means there's no error.
-        roll=list(recieved)                     ##### Roll is the first reading, we just remove the "," and "." and we have an integer reading
-        del roll[-1]                            ##### Delete last index of reading ","
-        del roll[-1]                            ##### Delete last index again "."
-        roll = "".join(roll)
-    else:
-        pitchyaw = recieved                     ##### Put the recieved reading into variable if it doesn't contain "."
-        pitch = pitchyaw.split(",")[0]          ##### The second and third reading are stuck to each other so this part splits them into two variables <=== Pitch
-        yaw = pitchyaw.split(",")[1]            ##### <=== Yaw
-    if len(roll)<6 and delayer>2000 :           ##### len(roll)<6 is to ensure that the reading has no error, because sometimes they get stuck into eachother so we disregard these readings. Delayer to wait a few seconds before taking readings to avoid error.
-        print([roll,pitch,yaw])
-    delayer+=1
- 
+    x = takeReadings()    ### This list contains the roll and pitch and yaw.
