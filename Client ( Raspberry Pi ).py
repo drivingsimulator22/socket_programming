8665efef613d6b185e3c8eaa37d78daa36473bd3
VR Driving Simulator <101948671+drivingsimulator22@users.noreply.github.com>
2022-04-11 10:52:11 +0200

    Update Client ( Raspberry Pi ).py

--- Client ( Raspberry Pi ).py
+++ Client ( Raspberry Pi ).py
@@ -1,17 +1,3 @@
-###### Functions Definitions:
-#######################################################################
-# Function Name: clientConnect() 
-# Description:
-# function is used to connect to the server from the raspberry pi
-# to the computer. This is implemented using the built-in python socket module.
-# We do this by defining a socket with an unused port. and connecting to the PC's
-# IP address.
-#######################################################################
-# Function Name: takeReadings() 
-# function takes the readings sent from the PC server from SIMTOOLS.
-# It then slices the recieved readings to get the roll,pitch and yaw angles.
-# the angles are then returned to be used in calculations later.
-######
 import socket
 def clientConnect():
     # Create a socket object
@@ -20,21 +6,25 @@ def clientConnect():
     # Define the port on which you want to connect
     port = 5050   
     # connect to the server on local computer
-    socketClient.connect(('192.168.130.220', port))
+    socketClient.connect(('192.168.130.203', port))
     print("Connected to host PC")
 def takeReadings():
     #Defining the variables            
     recieved = socketClient.recv(32768).decode()     ##### Put the recieved readings into variable
-    #### The initial reading sent from PC is in format: "0,0,0,"
-    #### so we split the string with "," as delimiter
-    #### and assign each value to the appropriate angle.
-    roll_recieved = recieved.split(",")[0]            ### Roll
-    pitch_recieved = recieved.split(",")[1]           ### Pitch
-    yaw_recieved = recieved.split(",")[2]             ### Yaw
-    ## The following if conditional is to remove all errors and noise
-    ## from our readings because sometimes we get two readings stuck together.
-    if int(yaw_recieved)<1024:                              
-        return [roll_recieved,pitch_recieved,yaw_recieved]
+    if recieved[0] == "R" and recieved.count("R")==1 and recieved[-1]=="," and len(recieved.split(","))==4: ## This line to ensure the reading doesn't come with errors
+        ## Making sure the first element is "R" and there's only one "R" also, the last element is "," and the length of the split string is equal to 4 ("R","P","Y"," ")
+        rollrecieved = recieved.split(",")[0].replace("R","")                                               ## Split the recieved string and remove R,P,Y.. Roll
+        pitchrecieved = recieved.split(",")[1].replace("P","")                                              ## Pitch
+        yawrecieved = recieved.split(",")[2].replace("Y","")                                                ## Yaw
+        final_list = [rollrecieved,pitchrecieved,yawrecieved]                                               ## Put the values into the final list
+        final_list = [int(x) for x in final_list]                                                           ## Convert list of strings into list of integers
+        # print(final_list)
+        return final_list
+
+### Input : Take readings from server
+
 clientConnect()
 while True:
-    x = takeReadings()    ### This list contains the roll and pitch and yaw.
+    readings = takeReadings()    ### This list contains the roll and pitch and yaw.
+    print(readings)
+### Output: Pitch, Yaw and Roll in a list of integers.
